cf356984e533f7376be315924d17b16959c1eacf
Vishwa1402 <gaurivalse123@gmail.com>
2026-05-16 03:08:18 +0530

    feat(auth): implement login, registration, JWT auth, route guards, and dashboard navigation

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,20 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient,  withInterceptors } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { jwtInterceptor }
+from './core/interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(FormsModule)
+    importProvidersFrom(FormsModule),
+    provideHttpClient(
+    withInterceptors([jwtInterceptor])
+)
   ]
 };
+
+
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 import { LoginComponent } from './features/auth/login/login';
 import { RegisterComponent } from './features/auth/register/register';
 import { DashboardComponent } from './features/dashboard/dashboard';
@@ -8,5 +9,14 @@ export const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  {
+  path: 'dashboard',
+  loadComponent: () =>
+    import('./features/dashboard/dashboard')
+      .then(m => m.DashboardComponent),
+
+  canActivate: [authGuard]
+}
 ];
+
+
